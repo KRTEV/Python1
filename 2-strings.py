eda44b6947695162fcc67e843bdcd134e532c10f
EAGG <sotonalu2012t@gmail.com>
2024-07-10 18:28:22 -0500

    complemento de ejercicios commit3

---
+++ 2-strings.py
@@ -0,0 +1,13 @@
+# print("Hola \"Jhoana\"" )
+
+# print("Esta es una linea\nY esta es otra linea")
+
+# print("\tEsta es una tabulacion")
+
+'''Practica 1 Strings'''
+
+print("Texto_1\nTexto_2\nTexto_3")
+
+'''Practica 2 Strings '''
+
+print("A\tB\tC\nD\tE\tF\nG\tH\tI")
---
+++ 3-input.py
@@ -0,0 +1,15 @@
+# input("Dime tu apodo: ")
+
+# print(input("Dime tu apodo: "))
+# print("Tu apodo es " + input("Dime tu apodo: "))
+
+'''Practica 1 Input'''
+
+input("¿Qué estás estudiando?")
+
+''' Practica 2 Input '''
+input("¿En que país vives?")
+
+''' Practica 3 Input '''
+
+print(input("Escribe tu tu nombre: ") + " " + input("Escribe tu apellido: "))
