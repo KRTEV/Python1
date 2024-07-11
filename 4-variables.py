8d41b9462c94f619b3bc7ca01ba547818bbe2be5
EAGG <sotonalu2012t@gmail.com>
2024-07-10 20:13:19 -0500

    Ejercicios con variables

---
+++ 4-variables.py
@@ -0,0 +1,23 @@
+# 1
+nombre = "Tony Soprano" 
+edad = 51  
+print("¿Como te llamas?\n Me llamo " + nombre)
+print("¿Cuantos años tienes? \n Tengo " , edad) 
+
+
+
+# 2
+
+nombre = "Julia"
+apellido = "Roberts"
+nombre_completo = nombre + " " + apellido
+
+print(nombre_completo)
+
+#3
+
+curso = 'Python'
+
+print("Estas tomando un curso de " + curso)
+ 
+
